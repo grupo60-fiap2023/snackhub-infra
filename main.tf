8243b789d851d41da31eb07b50e0b847d835cd1b
Igor Oliveira <igor_villanova@hotmail.com>
2023-11-03 08:43:09 -0300

    removido chaves inválidas da main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "AKIAZQ4WYSBP2Z3IE3P5"
-  secret_key = "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"
+  access_key = "acskey"
+  secret_key = "srckey"
   region     = var.region
 }
